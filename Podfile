1d367f4138b7f20264edab2c95b7f0640e84135a
Hassan Ali <hassan_ali9@outlook.com>
2022-03-14 18:29:10 +0200

    Sorting Files
    Sorting Files

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '12.0'
+
+target 'Shopify' do
+  use_frameworks!
+
+pod 'Alamofire', '~> 5.5'
+pod 'ReachabilitySwift'
+pod 'Kingfisher', '~> 7.0'
+pod 'Floaty', '~> 4.2.0'
+
+end
---
+++ Shopify/Constants/URLs/URLs.swift
@@ -0,0 +1,13 @@
+//
+//  URLs.swift
+//  Shopify
+//
+//  Created by Hassan on 14/03/2022.
+//
+
+import Foundation
+
+struct URLs{
+
+    let baseURL = "https://f36da23eb91a2fd4cba11b9a30ff124f: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@jets-ismailia.myshopify.com/admin/api/2022-01/"
+}
