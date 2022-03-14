487a1289aed933b4df10b17cd68ab2672b185150
AhmedNasr <63086808+ahmednasr8898@users.noreply.github.com>
2022-03-14 22:11:31 +0200

    someChanges

--- Shopify/Constants/URLs/URLs.swift
+++ Shopify/Constants/URLs/URLs.swift
@@ -14,6 +14,6 @@ struct URLs{
     let baseURL = "https://f36da23eb91a2fd4cba11b9a30ff124f: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@jets-ismailia.myshopify.com/admin/api/2022-01/"
     
     func getAllBrandsURl()-> URL?{
-        return URL(string: "https://f36da23eb91a2fd4cba11b9a30ff124f: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@jets-ismailia.myshopify.com/admin/api/2022-01/smart_collections.json")
+        return URL(string: baseURL + "smart_collections.json")
     }
 }
