094874937fff744150c8fcd6366a6cf4465d6554
AhmedNasr <63086808+ahmednasr8898@users.noreply.github.com>
2022-03-19 23:20:38 +0200

    addProductIDInWishListModel

--- Shopify/Model/POJOs/WishListModel/WishListModel+CoreDataProperties.swift
+++ Shopify/Model/POJOs/WishListModel/WishListModel+CoreDataProperties.swift
@@ -16,9 +16,10 @@ extension WishListModel {
         return NSFetchRequest<WishListModel>(entityName: "WishListModel")
     }
 
-    @NSManaged public var customerID: String?
+    @NSManaged public var customerID: Int64
     @NSManaged public var productImage: String?
     @NSManaged public var productName: String?
     @NSManaged public var productPrice: String?
+    @NSManaged public var productID: Int64
 
 }
