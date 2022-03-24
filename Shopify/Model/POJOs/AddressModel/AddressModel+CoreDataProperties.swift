660e4bd80c02ff28d4afe36df422b1356119a4cc
Islam NourEldin <99802390+IslamNour93@users.noreply.github.com>
2022-03-23 19:24:27 -0700

    fix coredata files

--- Shopify/Model/POJOs/AddressModel/AddressModel+CoreDataProperties.swift
+++ Shopify/Model/POJOs/AddressModel/AddressModel+CoreDataProperties.swift
@@ -22,5 +22,5 @@ extension AddressModel {
     @NSManaged public var city: String?
     @NSManaged public var country: String?
     @NSManaged public var customerName: String?
-
+    @NSManaged public var phoneNumber: String?
 }
